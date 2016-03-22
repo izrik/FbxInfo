0035fbd9bc040f1c8662011053e9155c2b10e21e
izrik <izrik@izrik.com>
2016-03-22 12:04:36 -0500

    Import a scene.

--- FbxInfoForm.cs
+++ FbxInfoForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
+using FbxSharp;
 
 namespace FbxInfo
 {
@@ -26,6 +27,7 @@ namespace FbxInfo
 
             nodeHierarchy = new TreeView();
             nodeHierarchy.Dock = DockStyle.Fill;
+            nodeHierarchy.AfterSelect += NodeHierarchy_AfterSelect;
 
             output = new TextBox();
             output.ReadOnly = true;
@@ -55,14 +57,39 @@ namespace FbxInfo
             this.ResumeLayout(false);
             this.PerformLayout();
 
-            fileOpenItem = new MenuItem("Open", new EventHandler(File_Open_OnClick));
+            fileOpenItem = new MenuItem("Open", new EventHandler(FileOpenItem_Click));
             fileMenu = new MenuItem("File", new MenuItem[] { fileOpenItem });
             menu = new MainMenu(new MenuItem[] { fileMenu });
             this.Menu = menu;
         }
 
-        protected void File_Open_OnClick(object sender, EventArgs e)
+        void NodeHierarchy_AfterSelect(object sender, TreeViewEventArgs e)
         {
+            output.Text = e.Node.Text + "\n" + ((Scene)e.Node.Tag).ToString();
+        }
+
+        protected void FileOpenItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.Multiselect = false;
+            ofd.AddExtension = true;
+            ofd.DefaultExt = "fbx";
+            var result = ofd.ShowDialog(this);
+            if (result != DialogResult.OK) return;
+
+            var filename = ofd.FileName;
+            var importer = new Importer();
+            var scene = importer.Import(filename);
+
+            nodeHierarchy.Nodes.Add(GetNodeFromScene(scene));
+        }
+
+        TreeNode GetNodeFromScene(Scene scene)
+        {
+            var node = new TreeNode(scene.GetNameWithNameSpacePrefix());
+            node.Tag = scene;
+            return node;
         }
     }
 }
